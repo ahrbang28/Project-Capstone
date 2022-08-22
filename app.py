e337279ea1b3ad8137ef4f17624db2a2ce549106
Adetya <34619540+ahrbang28@users.noreply.github.com>
2022-08-22 21:41:14 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from PIL import Image
 st.set_page_config(page_title="Healthy Diet", layout="wide")
 
 dc = pd.read_csv('data_capstone_clean.csv')
+da = dc
+da['rank'] = da.groupby('year')['percentage'].rank(method='max')
 dc_gdp20lebih = dc.loc[(dc['gdp_per_capita'] >= 20000) & (dc['year'] == 2017)]
 dc_gdp20kurang = dc.loc[(dc['gdp_per_capita'] < 20000) & (dc['year'] == 2017)]
 data_2017 = dc[dc['year'] == 2017]
@@ -15,6 +17,7 @@ data_2018 = dc[dc['year'] == 2018]
 data_2019 = dc[dc['year'] == 2019]
 data_2020 = dc[dc['year'] == 2020]
 indonesia = dc[dc['country'].str.contains('Indonesia')]
+sea = dc[dc['country'].str.contains('Indonesia|Malaysia|Singapore|Thailand|Philippines|Vietnam|Myanmar|Cambodia')]
 #dc_top = dc.sort_values(by=['percentage','years'], ascending=True).head(10)
 #dc_top17 = dc[dc['year'] == 2017].sort_values(by='percentage', ascending=True).head(10)
 
@@ -95,18 +98,24 @@ st.write(" ")
 col1, col2 = st.columns([3,2])
 
 with col1:
-    tab1, tab2, tab3 = st.tabs(['GDP per capita < $20000','GDP per capita >= $20000','Trend Line'])
-    with tab1:
+    tab1, tab2, tab3, tab4 = st.tabs(['Rank World','GDP per capita < $20000','GDP per capita >= $20000','Trend Line'])
+    with tab1:      
+        fig = px.line(da, x='year', y='rank', color='country')
+        fig.update_layout(yaxis=dict(range=[0,150]))
+        fig.update_traces(mode="markers+lines")
+        fig.update_xaxes(showgrid=False)
+        st.write(fig)
+    with tab2:
         fig = px.pie(dc_gdp20kurang, values=dc_gdp20kurang['category'].value_counts().values, names=dc_gdp20kurang['category'].value_counts().index, labels={'names':'Category','values':'Count'})
         fig.update_traces(textfont_size=20)
         fig.update_layout(margin=dict(t=50, b=50, l=50, r=200))
         st.write(fig)
-    with tab2:
+    with tab3:
         fig = px.pie(dc_gdp20lebih, values=dc_gdp20lebih['category'].value_counts().values, names=dc_gdp20lebih['category'].value_counts().index, labels={'names':'Category','values':'Count'})
         fig.update_traces(textfont_size=20)
         fig.update_layout(margin=dict(t=50, b=50, l=50, r=200))
         st.write(fig)
-    with tab3:
+    with tab4:
         fig2 = sns.lmplot(data=dc, x='gdp_per_capita', y='Yearly_cost_healthy_diet', hue='year')
         plt.title('Pengeluaran Healthy Diet terhadap Total Pendapatan')
         st.pyplot(fig2)
@@ -115,7 +124,10 @@ with col2:
         st.write(" ")
         st.write(" ")
         st.write(" ")
-        st.markdown("""Terdapat trend bahwa Semakin besar total pendapatan, nilai dari pengeluaran Healthy Diet semakin kecil.
+        st.markdown("""Pada bagain rank world disamping terlihat secara sekilas terdapat 2 negara yang peringkatnya mengalami penurunan secara signifikan yaitu sudan dan sint maarten.
+        Pada tahun 2017 sudan berada pada peringkat 102 dan sint maarten pada peringkat 36. Ditahun-tahun selanjutnya peringkat kedua negara tersebut terus turun dan pada tahun 2020 sudan berada pada peringkat 141 dan sint maarten pada peringkat 88. 
+        """)
+        st.markdown("""Terdapat trend bahwa Semakin besar total pendapatan, Maka nilai dari pengeluaran Healthy Diet semakin kecil atau murah.
         """)
         st.markdown("""Lebih dari 70% negara memiliki total pendapatan dibawah $20.000, Dalam 70% negara terebut terdapat 50% negara yang pengeluaran Healthy Dietnya tergolong MAHAL dan 50% negara memiliki pengeluaran Healthy Diet yang tergolong MURAH.
         """)
@@ -137,24 +149,32 @@ with col1:
     st.write("")
     st.write("")
     st.markdown("""
+    Indonesia berada pada peringkat 101 pada tahun 2017 dan pada tahun tahun selanjutnya peringkat indonesia mengalami kenaikan hingga pada tahun 2020 indonesia berada pada peringkat 98 dari total 146 negara. Selain itu indonesia berada pada peringkat 4 dari total 8 negara yang berada di wilayah asia tenggara yang tercatat.\n
     Indonesia menjadi salah satu negara yang berkategori Expensive selama tahun 2017 - 2020 dengan nilai 41.5 pada tahun 2020. Pada tahun 2019 indonesia sempat berada pada nilai 37.1 yang nilai tersebut 2 lebih rendah dari pada tahun 2018 yang bernilai 39.5 dan 4 lebih rendah dari pada tahun 2020 yang bernilai 41.5.\n
-    Indonesia sendiri memiliki pengeluaran healthy diet berkisar pada harga \$4.2 dan pengeluaran rata-rata setiap tahunnya menghabiskan sebanyak \$1500.\n
+    Indonesia sendiri memiliki pengeluaran healthy diet berkisar pada harga \$4.2 per hari dan pengeluaran rata-rata setiap tahunnya menghabiskan sebanyak \$1500.\n
     Di indonesia pengeluaran Healthy Diet tiap tahun mengalami kenaikan sekitar 3%. Adapun persentase pengeluaran Healthy Diet terhadap Total Pendapatan per kapita di Indonesia mencapai rata-rata 39.5%, dan mengalami kenaikan rata-rata sekitar 1.5% per tahun.\n
     Dengan nilai persentase rata-rata berada di angka 39.5% ini membuat negara indonesia memiliki kategori Expensive.
     """)
 
 with col2:
-    tab1,tab2 = st.tabs(['Bar Plot','Line Plot'])
+    tab1,tab2,tab3 = st.tabs(['Rank Indonesia','Bar Plot','Line Plot'])
     with tab1:
+        fig = px.line(sea, x='year', y='rank', color='country')
+        fig.update_layout(yaxis=dict(range=[0,150]))
+        fig.update_traces(mode="markers+lines")
+        fig.update_xaxes(showgrid=False)
+        st.write(fig)
+    with tab2:
         fig = px.bar(indonesia, x='year' , y='percentage', text_auto=True)
         fig.update_yaxes(range=[0,100])
         st.write(fig)
-    with tab2:
+    with tab3:
         fig = px.line(indonesia, x='year', y=['gdp_per_capita','households_expenditure_per_capita','Yearly_cost_healthy_diet'], labels={'y':'Value ($)'} )
         fig.update_layout(yaxis=dict(range=[0,4500]))
         fig.update_traces(mode="markers+lines")
         fig.update_xaxes(showgrid=False)
         st.write(fig)
+
 
 
 #col1, col2, col3= st.columns(3)
@@ -183,9 +203,9 @@ with tab1:
         st.header(" ")
         st.header(" ")
         st.markdown("""
-        Pada tahun 2017 dapat dilihat pada pie chart bahwa sebanyak 36.3% atau 53 negara berada pada kategori Very Cheap dan 26% atau 38 negara berada pada kategori Very Expensive. 
-        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive berjumlah 54 (37%) 
-        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium berjumlah 92 (63%).
+        Pada tahun 2017 dapat dilihat pada pie chart terdapat 146 negara dan sebanyak 36.3% atau 53 negara berada pada kategori Very Cheap dan 26% atau 38 negara berada pada kategori Very Expensive. 
+        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive yang jumlahnya mencapai 54 negara(37%). 
+        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium yang jumlahnya mencapai 92 negara(63%).
         Sehingga dapat disimpulkan mayoritas negara memiliki kategori Healthy Diet yang tergolong MURAH.
         """)
 
@@ -201,9 +221,9 @@ with tab2:
         st.header(" ")
         st.header(" ")
         st.markdown("""
-        Pada tahun 2018 dapat dilihat pada pie chart bahwa sebanyak 38.2% atau 55 negara berada pada kategori Very Cheap dan 27.1% atau 39 negara berada pada kategori Very Expensive. 
-        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive berjumlah 51 (35.4%) 
-        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium berjumlah 93 (64.6%).
+        Pada tahun 2018 dapat dilihat pada pie chart terdapat 144 negara dan sebanyak 38.2% atau 55 negara berada pada kategori Very Cheap dan 27.1% atau 39 negara berada pada kategori Very Expensive. 
+        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive yang jumlahnya mencapai 51 negara(35.4%) 
+        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium yang jumlahnya mencapai 93 negara(64.6%).
         Sehingga dapat disimpulkan mayoritas negara memiliki kategori Healthy Diet yang tergolong MURAH.
         """)
 
@@ -219,9 +239,9 @@ with tab3:
         st.header(" ")
         st.header(" ")
         st.markdown("""
-        Pada tahun 2019 dapat dilihat pada pie chart bahwa sebanyak 36.8% atau 53 negara berada pada kategori Very Cheap dan 26.4% atau 38 negara berada pada kategori Very Expensive. 
-        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive berjumlah 52 (36.1%) 
-        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium berjumlah 92 (63.9%).
+        Pada tahun 2019 dapat dilihat pada pie chart terdapat 144 negara dan sebanyak 36.8% atau 53 negara berada pada kategori Very Cheap dan 26.4% atau 38 negara berada pada kategori Very Expensive. 
+        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive yang jumlahnya mencapai 52 negara(36.1%) 
+        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium yang jumlahnya mencapai 92 negara(63.9%).
         Sehingga dapat disimpulkan mayoritas negara memiliki kategori Healthy Diet yang tergolong MURAH.
         """)
 
@@ -237,9 +257,9 @@ with tab4:
         st.header(" ")
         st.header(" ")
         st.markdown("""
-        Pada tahun 2020 dapat dilihat pada pie chart bahwa sebanyak 33.3% atau 48 negara berada pada kategori Very Cheap dan 27.8% atau 40 negara berada pada kategori Very Expensive. 
-        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive berjumlah 55 (38.2%) 
-        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium berjumlah 89 (61.8%).
+        Pada tahun 2020 dapat dilihat pada pie chart terdapat 144 negara dan sebanyak 33.3% atau 48 negara berada pada kategori Very Cheap dan 27.8% atau 40 negara berada pada kategori Very Expensive. 
+        Negara yang memiliki kategori Healthy Diet mahal yaitu negara yang berada pada kategori expensive dan very expensive yang jumlahnya mencapai 55 negara(38.2%) 
+        dan negera yang memiliki Healthy Diet murah yaitu negara yang berada pada kategori Very Cheap, Cheap, dan Medium yang jumlahnya mencapai 89 negara(61.8%).
         Sehingga dapat disimpulkan mayoritas negara memiliki kategori Healthy Diet yang tergolong MURAH.
         """)
 
